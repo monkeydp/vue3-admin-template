cc6bb4cbaf5b4726a115d7d508ede22af0e94f79
iPotato <monkeydpotato@gmail.com>
2022-10-24 16:48:03 +0800

    Layout

---
+++ src/layout/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="layout">
+    <div class="navbar">
+      导航
+    </div>
+
+    <div class="content">
+      <div class="content-header">头部</div>
+      <main class="content-main">
+        <router-view/>
+      </main>
+      <div class="content-footer">底部</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-facing-decorator'
+
+@Component({})
+export default class Layout extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+.layout {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  color: white;
+
+  .navbar {
+    width: 200px;
+    background: blue;
+  }
+
+  .content {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+
+    &-header {
+      height: 40px;
+      background: green;
+    }
+
+    &-main {
+      flex: 1;
+      color: black;
+    }
+
+    &-footer {
+      height: 25px;
+      background: purple;
+    }
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
+import Layout from "@/layout/index.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: ()=>import('@/views/home/index.vue')
-    },
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'Root',
+            component: Layout,
+            redirect: '/home',
+            children: [
+                {
+                    path: '/home',
+                    name: 'Home',
+                    component: () => import('@/views/home/index.vue'),
+                },
+            ]
+        },
+    ]
 })
 
 export default router
