ca426c9cf7699c31089d11c9b0328229e8b97353
iPotato <monkeydpotato@gmail.com>
2022-10-24 17:51:11 +0800

    安装 @ant-design/icons-vue, 并全局注册

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,16 @@ import router from './router'
 import 'ant-design-vue/dist/antd.css'
 import '@/styles/index.scss'
 
+import * as Icons from '@ant-design/icons-vue'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+const icons: any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i])
+}
 
 app.mount('#app')
