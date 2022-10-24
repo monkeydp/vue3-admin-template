4474f2da0168ab655093eab74b5be5f5acaa4df8
iPotato <monkeydpotato@gmail.com>
2022-10-24 16:26:29 +0800

    安装 unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
@@ -12,6 +13,71 @@ export default defineConfig({
     plugins: [
         vue(),
         vueJsx(),
+        AutoImport({
+            // targets to transform
+            include: [
+                /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+                /\.vue$/, /\.vue\?vue/, // .vue
+                /\.md$/, // .md
+            ],
+
+            // global imports to register
+            imports: [
+                // presets
+                'vue',
+                'vue-router',
+                // custom
+                {
+                    '@vueuse/core': [
+                        // named imports
+                        'useMouse', // import { useMouse } from '@vueuse/core',
+                        // alias
+                        ['useFetch', 'useMyFetch'], // import { useFetch as useMyFetch } from '@vueuse/core',
+                    ],
+                    'axios': [
+                        // default imports
+                        ['default', 'axios'], // import { default as axios } from 'axios',
+                    ],
+                    '[package-name]': [
+                        '[import-names]',
+                        // alias
+                        ['[from]', '[alias]'],
+                    ],
+                },
+            ],
+
+            // Auto import for module exports under directories
+            // by default it only scan one level of modules under the directory
+            dirs: [
+                // './hooks',
+                // './composables' // only root modules
+                // './composables/**', // all nested modules
+                // ...
+            ],
+
+            // Filepath to generate corresponding .d.ts file.
+            // Defaults to './auto-imports.d.ts' when `typescript` is installed locally.
+            // Set `false` to disable.
+            dts: './auto-imports.d.ts',
+
+            // Auto import inside Vue template
+            // see https://github.com/unjs/unimport/pull/15 and https://github.com/unjs/unimport/pull/72
+            vueTemplate: false,
+
+            // Custom resolvers, compatible with `unplugin-vue-components`
+            // see https://github.com/antfu/unplugin-auto-import/pull/23/
+            resolvers: [
+                /* ... */
+            ],
+
+            // Generate corresponding .eslintrc-auto-import.json file.
+            // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
+            eslintrc: {
+                enabled: false, // Default `false`
+                filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+                globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+            },
+        }),
         Components({
             resolvers: [
                 AntDesignVueResolver(),
